c2a3adfdd1d0de128e499510dcba7692eb87e792
Darya-Viunyk <dar.viunyk@gmail.com>
2023-06-03 22:47:11 +0300

    Added task 6

--- server.js
+++ server.js
@@ -2,15 +2,14 @@
 // YrffHgFXVzEVfGWe
 
 const mongoose = require("mongoose");
-
+const { DB_HOST, PORT = 3000 } = process.env;
 const app = require("./app");
-const DB_HOST =
-  "mongodb+srv://daria:YrffHgFXVzEVfGWe@cluster0.2oqsugj.mongodb.net/me_contact?retryWrites=true&w=majority";
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => console.log(`Server started on port: ${3000}`));
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
   })
   .catch((error) => {
     console.log(error.message);
